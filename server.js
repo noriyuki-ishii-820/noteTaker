72d041b29e7248bba0a1ac73c8f3310ce986845a
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-10-27 18:14:28 +1100

    Express server

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 4000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// open index.html upon click on the start button
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// open notes.html upon click on the start button
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT : http://localhost:" + PORT);
+});
