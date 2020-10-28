40987ed5f5362911605092e090278f6621b4514e
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-10-28 12:43:48 +1100

    syntax error fixed

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,13 +5,15 @@ var path = require("path");
 // routing
 
 module.exports = function (app) {
-  app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-  });
-
   // open notes.html upon click on the start button
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  // once the get started button is clicked, loads the notes.html
+
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
   // If no matching route is found default to home
