c4f2491377f23a8de5690268b1049032d124647e
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-10-28 11:26:33 +1100

    /* routing fixed

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var fs = require("fs");
 var app = express();
 var PORT = 4000;
 
+var notes = "";
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,14 +30,27 @@ app.get("/", function (req, res) {
 
 app.get("/api/notes", function (req, res) {
   fs.readFile("./db/db.json", function (error, content) {
-    const notes = JSON.parse(content);
+    let notes = JSON.parse(content);
     return res.json(notes);
   });
 });
 
+// POST `/api/notes` - Should receive a new note to save on the request body,
+// add it to the `db.json` file, and then return the new note to the client.
+
+// app.post("/api/notes", function (req, res) {
+//   var newNotes = req.body;
+
+//   console.log(newNotes);
+
+//   notes.push(newNotes);
+
+//   res.json(newNotes);
+// });
+
 // open index whenever something not defined is in the URL
-app.get("*", function (req, res) {
-  fs.readFile(__dirname, "./public/index.html");
+
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
